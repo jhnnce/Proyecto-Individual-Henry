fc3c7d3628b06462d0289ca70c85d92a1ad8f27e
jhnnce <95657074+jhnnce@users.noreply.github.com>
2024-11-05 12:28:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -321,5 +321,5 @@ def recomendacion(titulo: str):
 
 if __name__ == '__main__':
     import uvicorn
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host='0.0.0.0', port=port)
