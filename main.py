23e9a94d5c775fbd1379b850cb2ac698594a567a
jhnnce <95657074+jhnnce@users.noreply.github.com>
2024-11-05 14:27:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -287,7 +287,3 @@ def recomendacion(titulo: str):
         
     else:
         return {'message':f'La pelicula {titulo}, No existe'}
-if __name__ == '__main__':
-    import uvicorn
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host='0.0.0.0', port=port)
